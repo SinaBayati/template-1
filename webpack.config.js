d5b32b3e9809efa2e91f1c9b4be55ef73619dccf
Sina Bayati <bayati.sina.w@gmail.com>
2024-05-24 05:14:13 +0330

    Add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,18 @@ module.exports = {
         test: /\.(jpg|jpeg|svg|png|gif)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
     ],
   },
 };
